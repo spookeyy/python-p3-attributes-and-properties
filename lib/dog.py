0355225ecc85eb66b394ad9cc99a02037ad0b5c9
spookeyy <pangasmeshack@gmail.com>
2024-05-26 02:53:51 +0300

    testing

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,12 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="", breed="Chihuahua"):
+        self.name = name
+        self.breed = breed
+# check if it exists first
+        if self.breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds." )
+    # proceed and check if it is a string
+        elif self.name == "" or self.name != type(str):
+            print("Name must be string between 1 and 25 characters.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,16 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+
+    def __init__(self, name="", job="Admin"):
+        self.name = name
+        self.job = job
+
+        if self.job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+
+        elif self.name == "" or self.name != type(str):
+            print("Name must be string between 1 and 25 characters.")
+        elif len(self.name) >= 1 and len(self.name) <= 25:
+            self.name = name.title()
+
