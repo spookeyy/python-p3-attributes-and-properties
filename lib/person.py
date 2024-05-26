8b5f416762b2c531565c3a7d6654a461cc351753
spookeyy <pangasmeshack@gmail.com>
2024-05-26 12:23:07 +0300

    pass

--- lib/person.py
+++ lib/person.py
@@ -23,8 +23,10 @@ class Person:
 
         if self.job not in APPROVED_JOBS:
             print("Job must be in list of approved jobs.")
+        elif self.job in APPROVED_JOBS:
+            self.job = job
 
-        elif self.name == "" or self.name != type(str):
+        if self.name == "" or self.name != type(str):
             print("Name must be string between 1 and 25 characters.")
         elif len(self.name) >= 1 and len(self.name) <= 25:
             self.name = name.title()
